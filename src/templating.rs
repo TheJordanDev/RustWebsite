cb591c5faf1cda96b77509475eafd6647bab8624
Jordan <thejordan.dev@gmail.com>
2024-09-17 00:20:46 +0200

    Upgraded assets fetch

--- src/templating.rs
+++ src/templating.rs
@@ -5,18 +5,22 @@ pub struct TemplateHeaderData<'a> {
     styles: Option<Vec<&'a str>>,
 }
 
-pub fn render_header(data:&TemplateHeaderData) -> Markup {
+fn fetch_assets<S: AsRef<str>>(path: S) -> String {
     let timestamp = Utc::now().timestamp();
+    format!("{}?v={}", path.as_ref(), timestamp)
+}
+
+pub fn render_header(data:&TemplateHeaderData) -> Markup {
     html! {
         head {
             meta charset="UTF-8";
             title { (data.title) }
             link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet";
-            link rel="stylesheet" href=(format!("{}?v={}", "/assets/css/styles.css", timestamp))
+            link rel="stylesheet" href=(fetch_assets("/assets/css/styles.css"))
             meta name="viewport" content="width=device-width, initial-scale=1";
             @if let Some(styles) = &data.styles {
                 @for style in styles {
-                    link rel="stylesheet" type="text/css" href=(format!("{}?v={}", style, timestamp));
+                    link rel="stylesheet" type="text/css" href=(fetch_assets(format!("/assets/css/{}", style)));
                 }
             }
         }
@@ -30,7 +34,7 @@ pub fn render_home() -> Markup {
             (render_header(&TemplateHeaderData{title: "Home", styles: None}))
             body {
                 article {
-                    img src="/assets/images/warning.svg" alt="Warning" class="svg";
+                    img src=(fetch_assets("/assets/images/warning.svg")) alt="Warning" class="svg";
                     h1 {
                         "We'll be back soon!"
                         br;
